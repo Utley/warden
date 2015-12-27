6c08c64d8fd7135820e820ca7f6e5c4945a12748
Stephen Utley <stephen@utley.tech>
2015-12-27 01:22:44 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+</head>
+<body>
+  <h1>Latest matches:</h1>
+
+<?php
+$server = "localhost";
+$db = "warden";
+$user = "root";
+$pass = "#tlyps32";
+$conn = new mysqli($server,$user,$pass,$db);
+$getAllSql = "SELECT * FROM hero_names";
+$response = $conn->query($getAllSql);
+$heroesArray = [];
+$apiKey = "9AAC511CA9F6E38435866DD57DDDACA2";
+function getHeroesArray(){
+  $apiKey = "9AAC511CA9F6E38435866DD57DDDACA2";
+  $heroesArray = [];
+  $getHeroesUrl = "https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=" . $apiKey;
+  $ch = curl_init($getHeroesUrl);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);    # required for https urls
+  $heroesResult = curl_exec($ch);
+  curl_close($ch);
+  $heroesObject = json_decode($heroesResult);
+  $heroesObjectArray = $heroesObject->result->heroes;
+  foreach ($heroesObjectArray as $hero){
+    $heroesArray[$hero->id] = $hero->name;
+  }
+  return $heroesArray;
+};
+$heroesArray = getHeroesArray();
+$steamUrl = "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?format=JSON&matches_requested=2&key=" . $apiKey;
+$ch = curl_init($steamUrl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);    # required for https urls
+$head = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+$obj = json_decode($head);
+$latestMatches = $obj->result->matches;
+echo '<br>';
+for($i = 0; $i < sizeof($latestMatches); $i++){
+  echo "<a href='match.php?match=" . $latestMatches[$i]->match_id . "'>Match: " . $latestMatches[$i]->match_id . "</a>";
+  $mID = $latestMatches[$i]->match_id;
+
+  echo '<br>';
+  foreach ($latestMatches[$i]->players as $player){
+    $smallName = str_replace("npc_dota_hero_","",$heroesArray[$player->hero_id]);
+
+    echo "Hero: " . $smallName;
+    echo '<img src="' . 'http://cdn.dota2.com/apps/dota2/images/heroes/' . $smallName . '_sb.png"/>';
+    echo '<br>';
+  }
+  echo '<br>';
+}
+echo '<br>';
+?>
+</body>
+</html>
---
+++ match.php
@@ -0,0 +1,132 @@
+<html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" />
+  <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+</head>
+<body>
+<form action="match.php">
+  <input type="text" name="match" id="matchId" />
+  <input type="submit" />
+</form>
+
+<?php
+$matchID = $_REQUEST['match'];
+function getHeroesArray(){
+  $apiKey = "9AAC511CA9F6E38435866DD57DDDACA2";
+  $heroesArray = [];
+  $getHeroesUrl = "https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=" . $apiKey;
+  $ch = curl_init($getHeroesUrl);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);    # required for https urls
+  $heroesResult = curl_exec($ch);
+  curl_close($ch);
+  $heroesObject = json_decode($heroesResult);
+  $heroesObjectArray = $heroesObject->result->heroes;
+  foreach ($heroesObjectArray as $hero){
+    $heroesArray[$hero->id] = $hero->name;
+  }
+  return $heroesArray;
+};
+function getItemsArray(){
+  $apiKey = "9AAC511CA9F6E38435866DD57DDDACA2";
+  $heroesArray = [];
+  $getHeroesUrl = "https://api.steampowered.com/IEconDOTA2_570/GetGameItems/v0001/?key=" . $apiKey . "&language=en";
+  $ch = curl_init($getHeroesUrl);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);    # required for https urls
+  $heroesResult = curl_exec($ch);
+  curl_close($ch);
+  $heroesObject = json_decode($heroesResult);
+  $heroesObjectArray = $heroesObject->result->items;
+  foreach ($heroesObjectArray as $hero){
+    $heroesArray[$hero->id] = $hero->name;
+  }
+  return $heroesArray;
+};
+function getMatchDetails($matchID){
+  $apiKey = "9AAC511CA9F6E38435866DD57DDDACA2";
+  $detailsURL = "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?match_id=$matchID&key=$apiKey";
+  $ch = curl_init($detailsURL);
+
+  $optArray = array(
+    CURLOPT_RETURNTRANSFER => true
+  );
+
+  curl_setopt_array($ch, $optArray);
+
+  $response = curl_exec($ch);
+  $matchObject = json_decode($response);
+  $players = $matchObject->result->players;
+  $heroesArray = getHeroesArray();
+  $itemsArray = getItemsArray();
+  echo "<h1>" . $matchID . "</h1>";
+
+  echo "<table class='table table-striped'>";
+  echo "<tr>";
+  echo "<th></th>";
+  echo "<th>LH</th>";
+  echo "<th>D</th>";
+  echo "<th>XPM</th>";
+  echo "<th>GPM</th>";
+  echo "<th>HD</th>";
+  echo "<th>TD</th>";
+  echo "<th colspan=6>Items</th>";
+  echo "</tr>";
+  for($i = 0; $i < count($players); $i++){
+    $player = $players[$i];
+    if($i < count($players) / 2){
+      echo "<tr class='bg-danger'>";
+    }
+    else {
+      echo "<tr class='bg-success'>";
+    }
+    echo "<td>";
+    if(isset($players[$i]->hero_id)){
+      $smallName = str_replace("npc_dota_hero_","",$heroesArray[$players[$i]->hero_id]);
+      //echo "Hero: " . $smallName;
+      echo '<img src="' . 'http://cdn.dota2.com/apps/dota2/images/heroes/' . $smallName . '_sb.png"/>';
+
+    }
+    else{
+      echo "no hero;";
+    }
+    echo "</td>";
+    echo "<td>";
+    echo $players[$i]->last_hits;
+    echo "</td>";
+    echo "<td>" . $player->denies       . "</td>";
+    echo "<td>" . $player->xp_per_min   . "</td>";
+    echo "<td>" . $player->gold_per_min . "</td>";
+    echo "<td>" . $player->hero_damage  . "</td>";
+    echo "<td>" . $player->tower_damage . "</td>";
+
+    $items = [$player->item_0, $player->item_1, $player->item_2, $player->item_3, $player->item_4, $player->item_5];
+    for( $j = 0; $j < count($items); $j++ ){
+      $item = $itemsArray[$items[$j]];
+      $shortName = str_replace("item_","",$item);
+      if(substr_count("item_",$shortName) > -1){
+        echo "<td><img src='http://cdn.dota2.com/apps/dota2/images/items/" . $shortName . "_lg.png'></td>";
+      }
+      else{
+        echo "<td></td>";
+      }
+    }
+    echo "</tr>";
+  }
+
+  echo "</table>";
+  echo "<!--" . $response . "-->";
+
+}
+if(isset($matchID)){
+  getMatchDetails($matchID);
+}
+else
+{
+  getMatchDetails(2002487825);
+}
+?>
+</body>
+</html>
