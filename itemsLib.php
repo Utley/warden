b5d66b9daf9b08f2e2de8d3aa63e8a0b969b395e
Stephen Utley <utley2600@gmail.com>
2016-01-06 10:10:57 -0500

    moved getItemsArray to separate file

---
+++ itemsLib.php
@@ -0,0 +1,19 @@
+<?php
+function getItemsArray(){
+  global $apiKey;
+  $itemsArray = [];
+  $getItemsUrl = "https://api.steampowered.com/IEconDOTA2_570/GetGameItems/v0001/?key=" . $apiKey . "&language=en";
+  $ch = curl_init($getItemsUrl);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);    # required for https urls
+  $heroesResult = curl_exec($ch);
+  curl_close($ch);
+  $itemsObject = json_decode($heroesResult);
+  $itemsObjectArray = $itemsObject->result->items;
+  foreach ($itemsObjectArray as $item){
+    $itemsArray[$item->id] = $item->name;
+  }
+  return $itemsArray;
+};
+?>
