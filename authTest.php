89b5e39f1d254e7dd19fe089bae9d1d04c0da399
Stephen Utley <utley2600@gmail.com>
2016-01-21 10:15:17 -0500

    added basic steam auth through lightopenid php library

---
+++ authTest.php
@@ -0,0 +1,21 @@
+<?php
+include('openid.php');
+$key = trim(file_get_contents('/keys/apikey'));
+$openid = new LightOpenID('http://utley.tech/~stephen/warden/player.php');
+if(!$openid->mode){
+if(isset($_REQUEST['player'])){
+$openid->identity = 'http://steamcommunity.com/openid/';
+header('Location: ' . $openid->authUrl());
+}
+?>
+<form action="?player" method="post">
+<input type="image" src="http://cdn.steamcommunity.com/public/images/signinthroughsteam/sits_small.png">
+</form>
+<?php
+}
+else {
+echo $openid->validate() ? 'logged in' : 'failed';
+echo $openid->identity;
+}
+
+?>
